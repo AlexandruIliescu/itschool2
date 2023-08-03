4296621edba3677e0a5d83f78181d8f731d36fd6
Alex <alexandru.iliescu@fortechproducts.ro>
2023-08-03 18:48:17 +0300

    Commit from test branch

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         //comment from main branch
+        //comment from test branch
     }
 }

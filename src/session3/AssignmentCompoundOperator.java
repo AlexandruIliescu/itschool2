2c22d7b9d143be430ff0060eec00eb9b09cd3ec3
Alex <alexandru.iliescu@fortechproducts.ro>
2023-08-04 13:27:15 +0300

    Update AssignmentCompoundOperator class

--- src/session3/AssignmentCompoundOperator.java
+++ src/session3/AssignmentCompoundOperator.java
@@ -2,6 +2,9 @@ package session3;
 
 public class AssignmentCompoundOperator {
 
+    /*
+     *
+     */
     public static void main(String[] args) {
         int number = 15;
         number += 5;
@@ -9,5 +12,25 @@ public class AssignmentCompoundOperator {
 
         number *= 2;
         System.out.println("After multiply by 2: " + 2);
+
+
+        byte byteValue = 5;
+
+        /*
+         * Below example would lead to a compile error because
+         * Java automatically upgrades the result of an integer addition to an int.
+         */
+        //byteValue = byteValue + byteValue;
+
+        /*
+         * On the other hand, the example from below would implicitly cast the result back
+         * to the type of byteValue
+         */
+        byteValue += byteValue; //this is valid
+
+        /*
+         * Main advantage of compound assignment:
+         * - readability and conciseness: it makes coding a bit easier and more fluid
+         */
     }
 }
